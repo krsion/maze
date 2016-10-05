8d575b98ca7e6cdded968e768ae94f6ca9de35cf
Ondřej Krsička <o.krsicka@seznam.cz>
2016-10-05 17:36:30 +0200

    Higher order functionnnn
    Added forAllBases. It reduced redundant repeating the for loop.
    Last commit was a missclick.

--- maze/generator.js
+++ maze/generator.js
@@ -4,9 +4,26 @@ class Generator {
         this.cursor = cursor;
         this.base = base;
     }
+    //reduced repeating thx to this function
+    forAllBases(func) {
+        let x = this.area.sizes;
+        let y = this.area.sizes;
+        //need to use break in one func, did it via return bool value
+        let again = true;
+        for (let i = 2; i < y - 1; i += 2) {
+            for (let j = 2; j < x - 1; j += 2) {
+                if (again === false) {
+                    break;
+                }
+                //func needs indexes as arguments
+                again = func(j, i);
+            }
+        }
+    }
     prepare() {
         let x = this.area.sizes;
         let y = this.area.sizes;
+        //sidewalls
         for (let i = 0; i < y; i++) {
             for (let j = 0; j < x; j++) {
                 if (j === 0 || i === 0 || j === x - 1 || i === y - 1) {
@@ -14,43 +31,41 @@ class Generator {
                 }
             }
         }
-        for (let i = 2; i < y - 1; i += 2) {
-            for (let j = 2; j < x - 1; j += 2) {
-                this.area.setAt(j, i, this.base);
-            }
-        }
+        //bases
+        this.forAllBases((j, i) => {
+            this.area.setAt(j, i, this.base);
+            return true;
+        });
+        //initial and final point
         this.area.setAt(0, 1, this.area.ground);
         this.area.setAt(x - 1, y - 2, this.area.ground);
     }
     countBases() {
-        let x = this.area.sizes;
-        let y = this.area.sizes;
         let index = 0;
-        for (let i = 2; i < y - 1; i += 2) {
-            for (let j = 2; j < x - 1; j += 2) {
-                if (this.area.getAt(j, i) === this.base) {
-                    index += 1;
-                }
+        this.forAllBases((j, i) => {
+            if (this.area.getAt(j, i) === this.base) {
+                index += 1;
             }
-        }
+            return true;
+        });
         return index;
     }
     randomBase() {
         let num = Math.floor(Math.random() * this.countBases() + 1);
         let index = 0;
-        let x = this.area.sizes;
-        let y = this.area.sizes;
-        for (let i = 2; i < y - 1; i += 2) {
-            for (let j = 2; j < x - 1; j += 2) {
-                if (this.area.getAt(j, i) === this.base) {
-                    index += 1;
-                    if (index === num) {
-                        this.area.setAt(j, i, this.cursor);
-                        return [j, i];
-                    }
+        let xy = [];
+        this.forAllBases((j, i) => {
+            if (this.area.getAt(j, i) === this.base) {
+                index += 1;
+                if (index === num) {
+                    this.area.setAt(j, i, this.cursor);
+                    xy = [j, i];
+                    return false;
                 }
             }
-        }
+            return true;
+        });
+        return xy;
     }
     buildWall() {
         let dir = Math.floor(Math.random() * 4);
@@ -75,7 +90,7 @@ class Generator {
         this.prepare();
         let bases = this.countBases();
         while(bases > 0) {
-            
+
             this.buildWall();
             bases = this.countBases();
         }
