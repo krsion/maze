a20887fdeff6a6ba2c3ee73da4a6a3a7463b2add
Ondřej Krsička <o.krsicka@seznam.cz>
2016-10-05 17:33:15 +0200

    Higher order function
    Added forAllBases. It reduced redundant repeating the for loop.

--- maze/index.js
+++ maze/index.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    let area = new Area("steelblue", "wheat", 51, 10);
+    let area = new Area("steelblue", "wheat", 25, 10);
     let gen = new Generator(area, "yellowgreen", "red");
     gen.generate();
     area.draw();
